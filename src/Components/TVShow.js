13837864d268f950ea5bfb2edce41a67a4a0d62e
github username <github email address>
2019-06-08 16:09:12 -0400

    Done.

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -5,7 +5,14 @@ const tvShow = (props) => {
   return (
     <div>
       <br/>
-      <img src={props.show.image.medium} onClick={() => props.selectShow(props.show)} alt=""/>
+      <img
+          src={props.show.image ?
+              props.show.image.medium :
+              "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwik4OTd1triAhUkmu" +
+          "AKHVdUAhMQjRx6BAgBEAU&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F7995080%2Fhtml-if-image-is-not-found&" +
+          "psig=AOvVaw0VYhpjw5lVDTtwWWebkNKS&ust=1560110521343101"}
+          onClick={() => props.selectShow(props.show)} alt=""
+      />
     </div>
   );
 }
